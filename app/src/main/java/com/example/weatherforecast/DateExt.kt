5765f72946a7828754801d4ebf4847923898c94b
EpsilonChaos <DARKNESS256I@GMAIL.COM>
2023-09-01 17:34:39 +0300

    Cleanup

--- app/src/main/java/com/example/weatherforecast/DateExt.kt
+++ app/src/main/java/com/example/weatherforecast/DateExt.kt
@@ -7,6 +7,7 @@ const val FORMAT_EEE_d_MMMM_HH_mm = "EEE, d MMMM HH:mm"
 const val FORMAT_EEE_HH_mm = "EEE, HH:mm"
 const val FORMAT_HH_mm = "HH:mm"
 
+
 fun String.toTime(pattern: String) =
     SimpleDateFormat(pattern, Locale.getDefault()).parse(this)?.time ?: 0L
 
